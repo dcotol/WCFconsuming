ad0422676b1c012973b040348f6f957a42c72804
Douglas Coto <dcotolara@gmail.com>
2024-08-09 21:52:50 -0400

    Initial Commit

---
+++ WCFTestForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace WCFConsuming
+{
+  partial class WCFTestForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnExecute = new System.Windows.Forms.Button();
+      this.txtMessage = new System.Windows.Forms.TextBox();
+      this.lblResponseMsg = new System.Windows.Forms.Label();
+      this.btnCustomer = new System.Windows.Forms.Button();
+      this.rchResponse = new System.Windows.Forms.RichTextBox();
+      this.btnUpdate = new System.Windows.Forms.Button();
+      this.txtJson = new System.Windows.Forms.TextBox();
+      this.lblResponseUpdate = new System.Windows.Forms.Label();
+      this.lblTitle1 = new System.Windows.Forms.Label();
+      this.lblTitle2 = new System.Windows.Forms.Label();
+      this.btnToken = new System.Windows.Forms.Button();
+      this.rchToken = new System.Windows.Forms.RichTextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btnExecute
+      // 
+      this.btnExecute.Location = new System.Drawing.Point(716, 92);
+      this.btnExecute.Name = "btnExecute";
+      this.btnExecute.Size = new System.Drawing.Size(97, 39);
+      this.btnExecute.TabIndex = 0;
+      this.btnExecute.Text = "Run";
+      this.btnExecute.UseVisualStyleBackColor = true;
+      this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+      // 
+      // txtMessage
+      // 
+      this.txtMessage.Location = new System.Drawing.Point(94, 92);
+      this.txtMessage.Name = "txtMessage";
+      this.txtMessage.Size = new System.Drawing.Size(600, 31);
+      this.txtMessage.TabIndex = 1;
+      // 
+      // lblResponseMsg
+      // 
+      this.lblResponseMsg.AutoSize = true;
+      this.lblResponseMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblResponseMsg.Location = new System.Drawing.Point(88, 155);
+      this.lblResponseMsg.Name = "lblResponseMsg";
+      this.lblResponseMsg.Size = new System.Drawing.Size(76, 31);
+      this.lblResponseMsg.TabIndex = 2;
+      this.lblResponseMsg.Text = " Msg";
+      // 
+      // btnCustomer
+      // 
+      this.btnCustomer.Location = new System.Drawing.Point(1250, 92);
+      this.btnCustomer.Name = "btnCustomer";
+      this.btnCustomer.Size = new System.Drawing.Size(181, 39);
+      this.btnCustomer.TabIndex = 3;
+      this.btnCustomer.Text = "Get Customer";
+      this.btnCustomer.UseVisualStyleBackColor = true;
+      this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+      // 
+      // rchResponse
+      // 
+      this.rchResponse.Location = new System.Drawing.Point(908, 155);
+      this.rchResponse.Name = "rchResponse";
+      this.rchResponse.Size = new System.Drawing.Size(783, 230);
+      this.rchResponse.TabIndex = 4;
+      this.rchResponse.Text = "";
+      // 
+      // btnUpdate
+      // 
+      this.btnUpdate.Location = new System.Drawing.Point(94, 464);
+      this.btnUpdate.Name = "btnUpdate";
+      this.btnUpdate.Size = new System.Drawing.Size(181, 39);
+      this.btnUpdate.TabIndex = 5;
+      this.btnUpdate.Text = "Update Customer";
+      this.btnUpdate.UseVisualStyleBackColor = true;
+      this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+      // 
+      // txtJson
+      // 
+      this.txtJson.Location = new System.Drawing.Point(94, 204);
+      this.txtJson.Multiline = true;
+      this.txtJson.Name = "txtJson";
+      this.txtJson.Size = new System.Drawing.Size(719, 230);
+      this.txtJson.TabIndex = 6;
+      // 
+      // lblResponseUpdate
+      // 
+      this.lblResponseUpdate.AutoSize = true;
+      this.lblResponseUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblResponseUpdate.Location = new System.Drawing.Point(305, 464);
+      this.lblResponseUpdate.Name = "lblResponseUpdate";
+      this.lblResponseUpdate.Size = new System.Drawing.Size(82, 31);
+      this.lblResponseUpdate.TabIndex = 7;
+      this.lblResponseUpdate.Text = "Resp";
+      // 
+      // lblTitle1
+      // 
+      this.lblTitle1.AutoSize = true;
+      this.lblTitle1.Location = new System.Drawing.Point(89, 29);
+      this.lblTitle1.Name = "lblTitle1";
+      this.lblTitle1.Size = new System.Drawing.Size(83, 25);
+      this.lblTitle1.TabIndex = 8;
+      this.lblTitle1.Text = "Playing";
+      // 
+      // lblTitle2
+      // 
+      this.lblTitle2.AutoSize = true;
+      this.lblTitle2.Location = new System.Drawing.Point(89, 576);
+      this.lblTitle2.Name = "lblTitle2";
+      this.lblTitle2.Size = new System.Drawing.Size(147, 25);
+      this.lblTitle2.TabIndex = 9;
+      this.lblTitle2.Text = "Getting Token";
+      // 
+      // btnToken
+      // 
+      this.btnToken.Location = new System.Drawing.Point(94, 644);
+      this.btnToken.Name = "btnToken";
+      this.btnToken.Size = new System.Drawing.Size(181, 39);
+      this.btnToken.TabIndex = 10;
+      this.btnToken.Text = "Get Token";
+      this.btnToken.UseVisualStyleBackColor = true;
+      this.btnToken.Click += new System.EventHandler(this.btnToken_Click);
+      // 
+      // rchToken
+      // 
+      this.rchToken.Location = new System.Drawing.Point(335, 644);
+      this.rchToken.Name = "rchToken";
+      this.rchToken.Size = new System.Drawing.Size(1356, 188);
+      this.rchToken.TabIndex = 11;
+      this.rchToken.Text = "";
+      // 
+      // WCFTestForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1836, 863);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.rchToken);
+      this.Controls.Add(this.btnToken);
+      this.Controls.Add(this.lblTitle2);
+      this.Controls.Add(this.lblTitle1);
+      this.Controls.Add(this.lblResponseUpdate);
+      this.Controls.Add(this.txtJson);
+      this.Controls.Add(this.btnUpdate);
+      this.Controls.Add(this.rchResponse);
+      this.Controls.Add(this.btnCustomer);
+      this.Controls.Add(this.lblResponseMsg);
+      this.Controls.Add(this.txtMessage);
+      this.Controls.Add(this.btnExecute);
+      this.Name = "WCFTestForm";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnExecute;
+    private System.Windows.Forms.TextBox txtMessage;
+    private System.Windows.Forms.Label lblResponseMsg;
+    private System.Windows.Forms.Button btnCustomer;
+    private System.Windows.Forms.RichTextBox rchResponse;
+    private System.Windows.Forms.Button btnUpdate;
+    private System.Windows.Forms.TextBox txtJson;
+    private System.Windows.Forms.Label lblResponseUpdate;
+    private System.Windows.Forms.Label lblTitle1;
+    private System.Windows.Forms.Label lblTitle2;
+    private System.Windows.Forms.Button btnToken;
+    private System.Windows.Forms.RichTextBox rchToken;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ WCFTestForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WCFConsuming
+{
+  public partial class WCFTestForm : Form
+  {
+    
+    public WCFTestForm()
+    {
+      InitializeComponent();
+    }
+
+    private void btnExecute_Click(object sender, EventArgs e)
+    {
+      //WCFService.DTEServiceClient client = new WCFService.DTEServiceClient("BasicHttpBinding_IDTEService");
+      //lblResponseMsg.Text = client.GetMessage(txtMessage.Text);
+
+      //WcfHttpsServiceProxy.ServiceProxyClient client = new WcfHttpsServiceProxy.ServiceProxyClient("WSHttpBinding_IServiceProxy3");
+      //lblResponseMsg.Text = client.GetMessage(txtMessage.Text);
+      //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRjb3RvbGFyYUBnbWFpbC5jb20iLCJuYmYiOjE3MjMwNzk1NzgsImV4cCI6MTcyMzE2NTk3OCwiaWF0IjoxNzIzMDc5NTc4LCJpc3MiOiJJbm5vdmEiLCJhdWQiOiJGYWN0dXJlZCJ9.pnXw4rWXmfSWHdQJXpCaLrMWRdc5pOthhOMoaAIM92A"
+//      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRjb3RvbGFyYUBnbWFpbC5jb20iLCJuYmYiOjE3MjMwODIwMDIsImV4cCI6MTcyMzE2ODQwMiwiaWF0IjoxNzIzMDgyMDAyLCJpc3MiOiJJbm5vdmEiLCJhdWQiOiJGYWN0dXJlZCJ9.UVJu5zfFFeRVDH6veCeR_aZQCVO0F-WqOF091izUxX4"
+
+      String UrlBuild = $"https://localhost:8443/ServiceProxy/GetMessage";
+      var client = new HttpClient();
+
+      var request = new HttpRequestMessage(HttpMethod.Post, $"{UrlBuild}");
+      //request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRjb3RvbGFyYUBnbWFpbC5jb20iLCJuYmYiOjE3MjMxMTQ1ODMsImV4cCI6MTcyMzIwMDk4MywiaWF0IjoxNzIzMTE0NTgzLCJpc3MiOiJJbm5vdmEiLCJhdWQiOiJGYWN0dXJlZCJ9.vE3RTNPTPdJ18QPqyw1TBTT3ytnYmeEEFc1dIvTQKIk");
+
+      request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("UserName", @"CLAVE-DELL13\Douglas");
+
+
+      var newMessa = new MsgRequest();
+      newMessa.Message = txtMessage.Text;
+
+      string jsonContent = JsonConvert.SerializeObject(newMessa);
+
+
+      StringContent content = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
+
+
+      //StringContent content = new StringContent(txtMessage.Text);
+
+      Task<HttpResponseMessage> responseTask = client.PostAsync(UrlBuild,content);
+
+      // Wait for the response and read the content
+      HttpResponseMessage response = responseTask.Result;
+      string responseBody = response.Content.ReadAsStringAsync().Result;
+
+      if (!String.IsNullOrEmpty(responseBody))
+        lblResponseMsg.Text = responseBody;
+      else
+        lblResponseMsg.Text = responseTask.Result.StatusCode.ToString();
+    }
+
+    private void btnCustomer_Click(object sender, EventArgs e)
+    {
+      WCFService.DTEServiceClient client = new WCFService.DTEServiceClient("BasicHttpBinding_IDTEService");
+      rchResponse.Text = client.GetCustomer();
+    }
+
+    private void btnUpdate_Click(object sender, EventArgs e)
+    {
+      WCFService.DTEServiceClient client = new WCFService.DTEServiceClient("BasicHttpBinding_IDTEService");
+      lblResponseUpdate.Text = client.UpdateCustomer(txtJson.Text);
+      rchResponse.Text = client.GetCustomer(); // Updating the view
+    }
+
+    private void btnToken_Click(object sender, EventArgs e)
+    {
+      //WCFService.DTEServiceClient client = new WCFService.DTEServiceClient("BasicHttpBinding_IDTEService");
+      //rchToken.Text = client.GetToken("{\"UserName\": \"dcotolara@gmail.com\", \"UserPassword\": \"Admin123@\"}");
+
+      WcfHttpsServiceProxy.ServiceProxyClient client = new WcfHttpsServiceProxy.ServiceProxyClient("WSHttpBinding_IServiceProxy3");
+      rchToken.Text = client.GetToken("{\"UserName\": \"dcotolara@gmail.com\", \"UserPassword\": \"Admin123@\"}");
+
+    }
+
+    public class MsgRequest
+    {
+      public String Message;
+    }
+
+  }
+
+}
